5cd42031e8303a70467be81413865364b490906a
Kevin Mugarura B <kevinmugarura051@gmail.com>
2024-06-30 07:18:23 +0300

    chore: Refactor FileStorage class to support filtering objects by class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+        else:
+            return {key: value
+                    for key, value in FileStorage.__objects.items()
+                    if value.__class__ == cls}
+        
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -24,27 +30,33 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
-
     def reload(self):
-        """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
+            """Loads storage dictionary from file"""
+            from os import path
+            from models.base_model import BaseModel
+            from models.user import User
+            from models.place import Place
+        
+            try:
+                temp = {}
+                with open(FileStorage.__file_path, 'r') as f:
+                    if path.getsize(FileStorage.__file_path) == 0:
+                        pass
+                    else:
+                        temp = json.load(f)
+                        for key, val in temp.items():
+                            self.all()[key] = classes[val['__class__']](**val)
+            except FileNotFoundError:
+                pass
 
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
-        try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
-        except FileNotFoundError:
+    def delete(self, obj=None):
+        """ Deletes an object from the file storage based on the name
+        """
+        if obj is None:
             pass
+        else:
+            objects_copy = FileStorage.__objects.copy()
+            for key, value in FileStorage.__objects.items():
+                if value.name == obj.name:
+                    del FileStorage.__objects[key]
+                    break
